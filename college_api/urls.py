a5b626ac95777d63b1647a0bba14ecc3d87bb154
ldomino <domino.laurie@gmail.com>
2022-04-28 14:38:26 -0500

    changes to urls for applicationtasks and naming consistency

--- college_api/urls.py
+++ college_api/urls.py
@@ -25,14 +25,12 @@ urlpatterns = [
     path('collegetkr/collegetrack/', TrackCollegeList.as_view(), name='college_track_show'),
     path('collegetkr/collegetrack/<int:pk>/', TrackCollegeDetail.as_view(), name='college_track_detail'),
 
+    # manytomany collegeToapplication routes
     path('collegetkr/collegeapps/<int:app_id>/all/', CollegeApplicationList.as_view(), name='collegeapp_index'),
     path('collegetkr/collegeapps/<int:pk>/', CollegeApplicationDetail.as_view(), name='collegeapp_detail'),    
-    path('collegetkr/apps/<int:college_id>/assign/<int:app_id>/', CollegeApplicationList.as_view(), name='collegeapp_assign_task'),
+    path('collegetkr/collegeapps/<int:college_id>/assign/<int:app_id>/', CollegeApplicationList.as_view(), name='collegeapp_assign_task'),
     path('collegetkr/collegeapps/<int:pk>/update/', CollegeApplicationDetail.as_view(), name='collegeapp_update'),
     path('collegetkr/collegeapps/<int:pk>/delete/', CollegeApplicationDetail.as_view(), name='colllegeapp_delete'),
-
-
-    # path('collegetkr/apps/<int:college_id>', views.apps_detail, name='app_detail'),
 
     # applications
     path('collegetkr/apps/', ApplicationList.as_view(), name='apps_index'),
@@ -41,18 +39,22 @@ urlpatterns = [
     path('collegetkr/apps/<int:pk>/update/', ApplicationDetail.as_view(), name='app_update'),
     path('collegetkr/apps/<int:pk>/delete/', ApplicationDetail.as_view(), name='app_delete'),
 
+    # tasks
     path('collegetkr/tasks/', TaskList.as_view(), name='task_index'),
     path('collegetkr/tasks/create/', TaskList.as_view(), name='create_task'),
     path('collegetkr/tasks/<int:pk>/update/', TaskDetail.as_view(), name='task_update'),
     path('collegetkr/tasks/<int:pk>/delete/', TaskDetail.as_view(), name='task_delete'),
     path('collegetkr/tasks/<int:pk>/', TaskDetail.as_view(), name='task_detail'),
 
-    path('collegetkr/tasks/<int:app_id>/', ApplicationTaskList.as_view(), name='app_task_index'),
+    #manytomany applicationtotask routes
+    path('collegetkr/apptasks/<int:app_id>/all/', ApplicationTaskList.as_view(), name='app_task_index'),
     path('collegetkr/apptasks/<int:app_id>/', ApplicationTaskList.as_view(), name='app_tasks_show'),
     #path('collegetkr/apptasks/', ApplicationTaskList.as_view(), name='app_tasks_show'),
     path('collegetkr/apptasks/<int:app_id>/assign/<int:task_id>/', ApplicationTaskList.as_view(), name='assign_task'),
-    path('collegetkr/apptasks/<int:pk>/', ApplicationTaskDetail.as_view(), name='remove_task'),
-  	# Restful routing
+    path('collegetkr/apptasks/<int:pk>/update/', ApplicationTaskDetail.as_view(), name='app_task_update'),
+    path('collegetkr/apptasks/<int:pk>/delete/', ApplicationTaskDetail.as_view(), name='app_task_remove'),
+
+  	# authentication
     path('signup/', SignUp.as_view(), name='sign-up'),
     path('login/', SignIn.as_view(), name='sign-in'),
     path('logout/', SignOut.as_view(), name='sign-out'),
